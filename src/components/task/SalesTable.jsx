47d921d8c2fbf7b98475201598400d008770ad48
Sabatr <bnguyen12152@gmail.com>
2019-08-17 18:03:30 +1200

    Added capitalisation

--- src/components/task/SalesTable.jsx
+++ src/components/task/SalesTable.jsx
@@ -9,6 +9,18 @@ import {
     Card, CardContent, Typography
 } from "@material-ui/core";
 
+/**
+ * An implementation of a sales table using ReactJS and Material-UI
+ * 
+ * My summary and findings:
+ * 
+ * When fetching the JSON data, I found out that I couldn't access it directly without it throwing an error. This is why
+ * I decided to check if it is null before adding it to my table. It was also quite strange when loading the data
+ * that the returned data did not show up. I'm assuming because it just thought of it as a boolean rather than a value to be displayed.
+ * 
+ * 
+ * @author Brian Nguyen
+ */
 class SalesTable extends React.Component {
     state = {
         data: []
@@ -28,26 +40,22 @@ class SalesTable extends React.Component {
                 <Table>
                     <TableHead>
                         <TableRow>
-                            {this.createHeadings().map((value,i) => <TableCell key={`heading_${i}`}>{value}</TableCell>)}
+                            {this.createHeadings().map((value,i) => 
+                                <TableCell key={`heading_${i}`}>
+                                    <div>{value}</div>
+                                </TableCell>)}
                         </TableRow>
                     </TableHead>
-    
                     <TableBody>
                     {
                     this.getData().map((data,i) =>
                         <TableRow key={`row_${i}`}>
                                 {Object.values(data).map((tableData,inc) => 
                                 <TableCell key={`cell_${inc}`}>
-                                    {console.log(inc + "and" + tableData)}
                                     <div>{`${tableData}`}</div>
                                 </TableCell>)}
                         </TableRow>)
                     }
-                    {/*
-                        exampleData.map((data, ind) => <TableRow key={`exampleRow_${ind}`}>
-                            {Object.values(data).map((d, i) => <TableCell key={`exampleCell_${i}`}>{d}</TableCell>)}
-                            </TableRow>)
-                    */}
                     </TableBody>
                 </Table>
             </CardContent>
@@ -60,9 +68,21 @@ class SalesTable extends React.Component {
      */
     createHeadings() {
         let headings = this.state.data.length === 0 ? ["loading..."] : Object.keys(this.state.data[0]);
-        return headings;
+        // I wanted to capitalise the headings because it looks nicer
+        let capitalisedHeadings = headings.map(heading => this.capitaliseFirstLetter(heading))
+        return capitalisedHeadings;
     }
 
+    /**
+     * Capitalised the first letter of a string
+     */
+    capitaliseFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    }
+
+    /**
+     * This function grabs the data from the json after it has loaded.
+     */
     getData() {
         let data = this.state.data.length === 0 ? ["loading..."] : Object.values(this.state.data);
         return data;
